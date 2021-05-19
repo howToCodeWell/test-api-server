192137100b2a447a09a8721caca84271b50b5745
pfwd <hello@websomatic.co.uk>
2021-05-19 15:38:10 +0100

    Adding Stripe and setup intents endpoint

---
+++ src/ResponseManager/StripeResponseManager.php
@@ -0,0 +1,31 @@
+<?php
+declare(strict_types=1);
+
+namespace App\ResponseManager;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\Yaml\Exception\ParseException;
+use Symfony\Component\Yaml\Yaml;
+
+class StripeResponseManager extends AbstractResponseManager implements ResponseManagerInterface
+{
+    public function create(Request $request, string $endPoint): ResponseConfig
+    {
+        $id = $request->get('intent_id');
+        $config = $this->getResponseConfig($endPoint, $id);
+        return new ResponseConfig($config['body'], $config['status_code']);
+    }
+
+
+    public function getResponseConfig(string $endPoint, string $filename): array
+    {
+        $path = '../config/responses/stripe/' . $endPoint . '/' . $filename . '.yaml';
+        try {
+            return Yaml::parseFile($path);
+        } catch (ParseException $exception) {
+            throw new NotFoundHttpException('Not Found '. $path);
+        }
+    }
+}
